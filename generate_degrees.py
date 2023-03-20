2b0ab8209d67ad989c2c6290b74f8d995a2ab183
David Viggiano <91092410+dviggiano@users.noreply.github.com>
2023-03-20 12:34:41 -0500

    Update generate_degrees.py

--- generate_degrees.py
+++ generate_degrees.py
@@ -3,6 +3,11 @@ from flask import jsonify
 
 
 def generate_degrees(courses_taken):
+    """
+    Unfortunately, we were unable to implement a fully functional algorithm given our limited timeframe.
+    This function provides a set of degrees for which the user has completed the most courses, though particular categories
+    and prerequisites are not fully accounted for.
+    """
     with open('data/majors.json') as f:
         majors = json.load(f)
 
